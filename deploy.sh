11cc37b8ac3a563c452a1905ffc1c4a9a1c1e942
youngzer <youngzer@qq.com>
2020-12-10 21:20:03 +0800

    解决自启服务阻塞问题

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-cp ./forwardbot.service /etc/systemd/system
 
-systemctl enable forwardbot.service
-
+echo "nohup python3 /root/discum/forward_bot.py > /dev/null 2>&1 &" >> /etc/rc.d/rc.local
